c2c7faef74b3ac22a8eb4f14c14be7c7317d220a
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-11-19 22:02:44 +0530

    Ready to use,Slide along with mouse and touch

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-console.log("Alex");
+const mySlider = document.querySelectorAll('.items');
+const slide = document.querySelector('.slider');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+const link = document.querySelector("a");
+
+mySlider.forEach(contents => contents.addEventListener('mousedown', () => {
+    isDown = true;
+    contents.classList.add('activate');
+    startX = e.pageX - slide.offsetLeft;
+    scrollLeft = slide.scrollLeft;
+}));
+mySlider.forEach(contents => contents.addEventListener('mouseup', () => {
+    isDown = false;
+    contents.classList.remove('activate');
+}));
+mySlider.forEach(contents => contents.addEventListener('mouseleave', () => {
+    isDown = false;
+    contents.classList.remove('activate');
+}));
+mySlider.forEach(contents => contents.addEventListener('mousemove', () => {
+    if (!isDown) { return; }
+    e.preventDefault();
+    const x = e.pageX - slide.offsetLeft;
+    const walk = (x - startX);
+    slide.scrollLeft = scrollLeft - walk;
+}));
+
+
+slide.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - slide.offsetLeft;
+    scrollLeft = slide.scrollLeft;
+});
+slide.addEventListener('mouseup', () => {
+    isDown = false;
+    
+});
+slide.addEventListener('mouseleave', () => {
+    isDown = false;
+    
+});
+slide.addEventListener('mousemove', (e) => {
+    if (!isDown) { return; }
+    e.preventDefault();
+    const x = e.pageX - slide.offsetLeft;
+    const walk = (x - startX)*4;
+    slide.scrollLeft = scrollLeft -walk;
+});
